65c6358ab494d54ccd129ae540bb18b39494eacf
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-07 08:20:11 -0300

    fix Text props

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -13,10 +13,10 @@ export interface TextProps {
 
 export const Text = styled(Typography)<TextProps>`
   font-weight: ${(props) =>
-    props.weight ? props.weight : props.theme.typography.fontWeightRegular};
+    props.weight ? props.weight : props.theme.typography.fontWeightRegular} !important;
   font-family: ${(props) =>
     props.font === 'Secondary' ? fontSecondary : props.theme.typography.fontFamily} !important;
   font-size: ${(props) => props.size || 1}rem !important;
   line-height: ${(props) => props.height || 1.43} !important;
-  text-align: ${(props) => props.align || 'auto'};
+  text-align: ${(props) => props.align || 'auto'} !important;
 `;
