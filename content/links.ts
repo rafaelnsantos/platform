b149e1ce5bd90cbca2e3e37a0f4b693bc076a18b
Andre Bonizi <andrebonizi@gmail.com>
2020-10-07 16:29:09 -0300

    social networks and email

--- content/links.ts
+++ content/links.ts
@@ -1,1 +1,5 @@
 const youtube = 'https://www.youtube.com/channel/UC1p7XPz5Ba25TQe2RSIOrzg';
+const instagram = 'https://instagram.com/redecardapio';
+const email = 'redecardapio@gmail.com';
+
+export { youtube, instagram, email };
--- src/components/molecules/mobile/FooterMobile.tsx
+++ src/components/molecules/mobile/FooterMobile.tsx
@@ -3,7 +3,8 @@ import { Animated, Text, Icon } from '@atoms';
 import { links } from 'content/mobile/headerMobile';
 import { FooterLinkMobile } from '@molecules/mobile/FooterLinkMobile';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
+import { faInstagram, faYoutube, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { youtube, instagram, email } from 'content/links';
 export const FooterMobileHeight = '15rem';
 
 const Container = styled.footer`
@@ -25,7 +26,7 @@ const StyledContact = styled.div`
   color: whitesmoke;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-evenly;
   padding-top: 30px;
 `;
 export const Footer = () => (
@@ -36,8 +37,14 @@ export const Footer = () => (
     <hr></hr>
     <Animated>
       <StyledContact>
-        <a href="mailto:redecardapio@gmail.com" target="blank">
+        <a href={`mailto:` + email} target="blank">
           <Icon icon={faEnvelope} size="lg"></Icon>
+        </a>
+        <a href={instagram} target="blank">
+          <Icon icon={faInstagram} size="lg"></Icon>
+        </a>
+        <a href={youtube} target="blank">
+          <Icon icon={faYoutube} size="lg"></Icon>
         </a>
       </StyledContact>
     </Animated>
